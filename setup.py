d15bde20de7fbbdafa9b14be471f70c777b7c820
bung87 <crc32@qq.com>
2025-08-19 14:47:07 +0800

    v0.1.5

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read_requirements():
 
 setup(
     name="k2edit",
-    version="0.1.4",
+    version="0.1.5",
     description="Terminal Code Editor with Kimi-K2 AI Integration",
     long_description=long_description,
     long_description_content_type="text/markdown",
